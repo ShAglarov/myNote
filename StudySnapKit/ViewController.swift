01971c2e9fd1f9c3d166ea1b6c4bae380f780975
Aglarov <shamil.aglarov@gmail.com>
2023-05-29 20:00:37 +0300

    Initial Commit

---
+++ StudySnapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudySnapKit
+//
+//  Created by Shamil Aglarov on 29.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
